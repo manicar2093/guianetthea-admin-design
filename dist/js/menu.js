835b2222b476394dcc0bbde7449fea713ca34766
Manuel Carbajal <manicar2093@gmail.com>
2021-03-25 14:16:28 -0600

    Se solucionan problemas de la vista en general. Se soluciona problema con la transición del menu al hacer toggle. Se agrega vista de bitacora

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
                 el.classList.toggle('is-active');
-                $target.classList.toggle('is-block');
+                $target.classList.toggle('invisible-on-touch');
 
             });
         });
